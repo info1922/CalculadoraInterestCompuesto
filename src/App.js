953fddc40ee6776efbba6ea8304b3d24ca185c63
ivan <ivann1922@gmail.com>
2021-12-09 18:03:46 -0600

    Calculadora de interes compuesto

---
+++ src/App.js
@@ -0,0 +1,85 @@
+
+import {useState} from 'react';
+import {Formik, Form} from 'formik'
+import * as Yup from 'yup';
+import Input from './components/Input'
+import Button from './components/Button'
+import Container from './components/Container'
+import Section from './components/Section'
+import Balance from './components/Balance'
+
+
+
+const compoundInterest = (deposito, contribution, years, rate) => {
+  let total = deposito;
+  
+  for (let i = 0; i < years; i++) {
+    total = (total + contribution) * (rate + 1)
+  }
+
+  return Math.round(total)
+
+}
+
+const formatter = new Intl.NumberFormat('en-US',  {
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2
+})
+
+function App() {
+
+  const [balance, setBalance] = useState('')
+
+  const handleSubmit = ({deposit, contribution, years, rate}) => {
+    const val = compoundInterest(
+      Number(deposit), Number(contribution), Number(years), Number(rate))
+      console.log(val);
+      setBalance(formatter.format(val))
+  }
+
+  return (
+    <Container>
+      <Section>
+        <Formik 
+          initialValues={{
+            deposit: '',
+            contribution: '',
+            years: '',
+            rate: ''
+          }}
+          onSubmit={handleSubmit}
+          validationSchema={Yup.object({
+            deposit: Yup.number().required('Obligatorio').typeError('Debe ser un numero'),
+            contribution: Yup.number().required('Obligatorio').typeError('Debe ser un numero'),
+            years: Yup.number().required('Obligatorio').typeError('Debe ser un numero'),
+            rate: Yup
+            .number()
+            .required('Obligatorio')
+            .typeError('Debe ser un numero')
+            .min(0, 'El valor minimo es 0')
+            .max(1, 'El valor maximo es 1'),
+          })}
+        >
+
+          <Form>
+            <Input name="deposit" label="Deposito inicial" />
+            <Input name="contribution" label="Contribucion anual" />
+            <Input name="years" label="Años" />
+            <Input name="rate" label="Intéres estimado" />
+            <Button type='submit'>Calcular</Button>
+          </Form>
+
+        </Formik>
+        {balance !== '' ? <Balance>Balance final: {balance} </Balance>  : null}
+      </Section>
+    </Container>
+  )
+}
+
+
+
+
+
+export default App
---
+++ src/components/Input.js
@@ -0,0 +1,46 @@
+
+
+import styled from 'styled-components'
+
+import {useField} from 'formik'
+
+const Control = styled.div`
+    margin-bottom: 20px;
+`
+
+const Label = styled.label`
+    color: #000;
+    margin-bottom: 5px;
+`
+
+const MyInput = styled.input`
+    outline: none;
+    padding: 8px;
+    border: solid 1px #b1b2b5;
+    border-radius: 4px;
+    width: 100%;
+    margin-bottom: 5px;
+`
+
+
+const ErrorMessage = styled.div`
+    color: #f00;
+`
+
+const  Input = ({label, ...props}) => {
+
+    const [field, meta] = useField(props)
+
+    return (
+        <Control>
+            <Label>{label}</Label>
+            <MyInput {...field} {...props} />
+            {meta.touched && meta.error ? 
+            <ErrorMessage>{meta.error}</ErrorMessage> : null}
+        </Control>
+    )
+
+}
+
+
+export default Input
